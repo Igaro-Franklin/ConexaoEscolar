a9f6f7c7d80f87fe8449070944ff1336fe0c05ac
Igaro-Franklin <igarofranklin@gmail.com>
2025-02-25 20:12:55 -0300

    Iniciou a tela home do projeto

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @turmas = Turma.includes(:alunos, :professor, :sala).order(:nome)
   end
 end
--- app/models/turma.rb
+++ app/models/turma.rb
@@ -1,3 +1,5 @@
 class Turma < ApplicationRecord
+  has_many :alunos, dependent: :destroy
   belongs_to :professor
+  has_one :sala, dependent: :destroy
 end
