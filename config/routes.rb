381794499b45c5fc259d0c418d2a590287c51358
Igaro-Franklin <igarofranklin@gmail.com>
2025-03-05 18:49:03 -0300

    Adicionou o login no projeto

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   root "pages#home"
 
   resources :alunos, only: [ :index, :new, :create, :edit, :update, :destroy ]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_10_134237) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_05_214209) do
   create_table "alunos", force: :cascade do |t|
     t.string "nome"
     t.date "data_nascimento"
@@ -46,6 +46,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_10_134237) do
     t.index ["turma_id"], name: "index_salas_on_turma_id"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
   create_table "turmas", force: :cascade do |t|
     t.string "nome"
     t.string "serie"
@@ -55,8 +64,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_10_134237) do
     t.index ["professor_id"], name: "index_turmas_on_professor_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
   add_foreign_key "alunos", "turmas"
   add_foreign_key "materia", "professors"
   add_foreign_key "salas", "turmas"
+  add_foreign_key "sessions", "users"
   add_foreign_key "turmas", "professors"
 end
